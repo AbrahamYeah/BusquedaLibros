949d9e3f2fddce6e1673830abe03b3d470b32996
AbrahamYeah <abrahamyeahmares@gmail.com>
2017-05-03 01:03:04 -0500

    Upadate

---
+++ BuesquedaLibros/app/src/main/java/com/example/abraham/buesquedalibros/NetworkUtils.java
@@ -0,0 +1,14 @@
+package com.example.abraham.buesquedalibros;
+
+/**
+ * Created by Abraham on 02/05/2017.
+ */
+
+class NetworkUtils {
+
+    public static final String BOOK_BASE_URL = "https://www.googleapis.com/books/v1/volumes?"; // la url de books API
+    public static final String CONSULTA_PARAM = "q"; //parametro para buscar
+    public static final String MAX_RESULTS = "maxRsults"; // parametro que limita los resultados de la busqueda
+    public static final String PRINT_TYPE = "printType"; // parametro a filtrar por el tipo
+
+}
